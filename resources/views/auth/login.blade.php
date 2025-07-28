a0d1ca743dbf9b70a5b9b3cc788cf3fef8103532
unknown <lisahumairoh01@gmail.com>
2025-07-28 18:31:57 +0700

    fix conflict

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,49 @@
 <!DOCTYPE html>
 <html lang="id">
-<head>
-  <meta charset="UTF-8">
-  <title>Login Admin</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="{{ asset('admin/assets/css/argon-dashboard-tailwind.css?v=1.0.1') }}" rel="stylesheet" />
-</head>
-<body style="background-color: #faf4dc;" class="m-0 font-sans antialiased font-normal text-start text-base leading-default text-slate-500">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('admin/assets/img/logo.png')}}" />
+    <link rel="icon" type="image/png" href="{{ asset('admin/assets/img/logo.png')}}" />
+    <title>Management Event Swipe</title>
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <!-- Nucleo Icons -->
+    <link href="{{ asset('admin/assets/css/nucleo-icons.css')}}" rel="stylesheet" />
+    <link href="{{ asset('admin/assets/css/nucleo-svg.css')}}" rel="stylesheet" />
+    <!-- Popper -->
+    <script src="https://unpkg.com/@popperjs/core@2'"></script>
+    <!-- Main Styling -->
+    <link href="{{ asset('admin/assets/css/argon-dashboard-tailwind.css?v=1.0.1')}}" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+
+    <style>
+        table.dataTable thead th {
+          @apply border-b bg-gray-100 text-xs font-bold uppercase text-slate-500 px-4 py-2;
+        }
+      
+        table.dataTable tbody td {
+          @apply px-4 py-2 text-slate-600;
+        }
+      
+        .dataTables_wrapper .dataTables_paginate .paginate_button {
+          @apply px-2 py-1 rounded-md border text-sm hover:bg-blue-100;
+        }
+      
+        .dataTables_wrapper .dataTables_length select,
+        .dataTables_wrapper .dataTables_filter input {
+          @apply border rounded px-2 py-1 text-sm;
+        }
+      
+        .dataTables_wrapper .dataTables_info {
+          @apply text-sm mt-2;
+        }
+      </style>
+      
+  </head>
+<body style="background-color: #ffffff;" class="m-0 font-sans antialiased font-normal text-start text-base leading-default text-slate-500">
 
   <main class="mt-0 transition-all duration-200 ease-in-out">
     <section>
